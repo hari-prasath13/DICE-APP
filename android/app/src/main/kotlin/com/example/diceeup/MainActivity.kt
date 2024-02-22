b67b63e22254ebdf31ce823bf4aac90a326dd05b
Hariprasath <muralihariprasath@gmail.com>
2024-02-22 12:43:55 +0530

    Comit final

---
+++ android/app/src/main/kotlin/com/example/diceeup/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.diceeup
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red[400],
+        appBar: AppBar(
+          backgroundColor: Colors.red[600],
+          title: Center(child: Text('DICEE')),
+        ),
+        body: dice(),
+      ),
+    ),
+  );
+}
+
+class dice extends StatefulWidget {
+  const dice({super.key});
+
+  @override
+  State<dice> createState() => _diceState();
+}
+
+class _diceState extends State<dice> {
+
+  int num = 1;
+  int numb = 1;
+
+  void blink() {
+    setState(() {
+      num =Random().nextInt(6) + 1;
+      numb = Random().nextInt(6) + 1;
+      print("CLICK");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                 blink();
+              },
+              child: Image.asset('images/dice$num.png'),
+            ),
+          ),
+          SizedBox(width: 10,),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                blink();
+              },
+              child: Image.asset('images/dice$numb.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
